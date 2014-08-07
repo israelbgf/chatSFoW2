237dfac61b2564f71f5c222b4fa1543a3ac9c551
diegofaria <diegofndefaria@gmail.com>
2014-08-06 23:49:27 -0300

    File name and directories refactoring

--- public/javascript/chatsfow2/command-facade.js
+++ public/javascript/chatsfow2/command-facade.js
@@ -9,16 +9,17 @@ $(function(){
 
 	$("#chatForm").on("submit", function(event) {
 		var input = $("#inputMessage").val();
-	    COMMANDS[findCommand(input)].execute(findData(input));
-	    event.preventDefault();
+		var command = extractCommand(input);
+		event.preventDefault();
+	    COMMANDS[command].execute(extractArgument(input));	    
 	});
 
-	function findCommand(input) {
+	function extractCommand(input) {
 		return input.charAt(0) == '!' ? input.split(" ")[0] : '!chat';
 	}
 
-	function findData (input) {
-		return input.split(" ")[1];
+	function extractArgument (input) {
+		return input.indexOf(" ") > -1 ? input.split(" ")[1] : input;
 	}
 
 })
