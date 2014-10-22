7b44c86a9fd474363528ccdc72523fe1c1a76824
Israel Fonseca <israel.bgf@gmail.com>
2014-10-22 01:40:55 -0200

    Removed unused libraries and code.

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ var path = require('path');
 
 //public modules from npm
 var express = require('express');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var favicon = require('static-favicon');
-var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
@@ -16,40 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 module.exports = app;
