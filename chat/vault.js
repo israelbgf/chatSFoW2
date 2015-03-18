8d809c372d46ce14a8a07792f0bef9df2305f246
Diego Faria <diegofndefaria@gmail.com>
2015-03-18 20:58:54 -0300

    implemented code to use configuration module

--- chat/vault.js
+++ chat/vault.js
@@ -1,5 +1,13 @@
-if (process.argv[2] == "mongodb"){
+var configuration = require('./configuration')
+var file = require('../config.json');
+var environment = process.env;
+
+
+var config = configuration.from(environment, file);
+if (config.persistence.provider == config.MONGODB){
+    console.log('Using mongo-vaults...');
     module.exports = require("./vault-mongodb");
 } else {
+    console.log('Using file-vaults...');
     module.exports = require("./vault-file");
 }
